61222b9f67a7806192a51f34b24b1545f085b63f
Jonny Lake <jonathanjlake@gmail.com>
2022-03-07 13:16:44 -0800

    updated packages

--- OPC-UA.py
+++ OPC-UA.py
@@ -1,32 +1,28 @@
-from multiprocessing.connection import Client
+
 import time
 import code
-from unicodedata import name
-from numpy import var
 
-from opcua import client
-from opcua import ua
+from opcua import Client, ua
+# def embed():
+#     vars = globals()
+#     vars.update(locals())
+#     shell = code.InteractiveConsole(vars)
+#     shell.interact()
 
-def embed():
-    vars = globals()
-    vars.update(locals())
-    shell = code.InteractiveConsole(vars)
-    shell.interact()
+# class SubHandler(object):
 
-class SubHandler(object):
-
-    def datachange_notification(self, node, val, data):
-        print("New data event at NodeID", node, ". New value = ", val)
+#     def datachange_notification(self, node, val, data):
+#         print("New data event at NodeID", node, ". New value = ", val)
 
 if __name__ == "__main__":
     # change stuff here
-    client = Client("opc.tcp://IP/")
+    client = Client("opc.tcp://192.168.0.1:4840/", timeout=10)
     
     try:
         client.connect()
 
         # change stuff here
-        var1_node = client.get_node('OPCUA stuff')
+        var1_node = client.get_node('ns=3;s="OPC"."PLCstate"')
         var1 = var1_node.get_value()
         var1_setValue = ua.DataValue(ua.Variant(not var1, var1_node.get_data_type_as_variant_type()))
         var1_node.set_value(var1_setValue)
